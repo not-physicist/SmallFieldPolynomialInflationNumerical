1d33757d8d5cd756c93300b5c72c69fef4d4e7cb
Chenhuan Wang <chenhuan@fastmail.com>
2021-12-08 15:09:31 +0100

    change function to rescaled unit; add functions in planck units

--- globals.py
+++ globals.py
@@ -1,18 +1,19 @@
 import numpy as np
 #  import matplotlib.pyplot as plt
 
-# all in reduced planck unit
 m_pl = 2.4e18  # GeV
 
 
-def get_Hubble_SL(phi, inf_model):
+######################################################################
+# in reduced planck unit
+def get_Hubble_SL_planck(phi, inf_model):
     '''
     return Hubble constant with slow-roll
     '''
     return np.sqrt(inf_model.get_V(phi)/3.0)
 
 
-def get_phi_dot_SL(phi, inf_model):
+def get_phi_dot_SL_planck(phi, inf_model):
     '''
     returns inflaton velocity with SL
     '''
@@ -20,8 +21,34 @@ def get_phi_dot_SL(phi, inf_model):
     return -inf_model.get_V_p(phi)/(3.0*H)
 
 
-def get_Hubble(phi, phi_dot, V):
+def get_Hubble_planck(phi, phi_dot, inf_model):
     '''
     get Hubble parameter, exact in homogeneous background
     '''
+    V = inf_model.get_V(phi)
     return np.sqrt((phi_dot**2/2.0 + V)/3.0)
+######################################################################
+
+
+######################################################################
+# in phi0, H0 planck unit
+def get_phi_dot_SL(phi, inf_model):
+    '''
+    rescaled phi dot in slow roll
+    '''
+    return get_phi_dot_SL(phi, inf_model) / inf_model.get_phi0()
+
+
+def get_Hubble_SL(phi, inf_model):
+    '''
+    rescale Hubble parameter in slow-roll
+    '''
+    return get_Hubble_SL(phi, inf_model) * inf_model.get_phi0()
+
+
+def get_Hubble(phi, phi_dot, inf_model):
+    '''
+    rescale Hubble parameter, exact
+    '''
+    return get_Hubble(phi, phi_dot, inf_model) * inf_model.get_phi0()
+######################################################################
