390bf42363fb10746f0f2893419853a882dade2f
Chenhuan Wang <chenhuan@fastmail.com>
2021-12-16 10:52:33 +0100

    correct function to ouput CL's parameters

--- parameters.py
+++ parameters.py
@@ -27,7 +27,7 @@ def get_Hubble_SR_para_planck(hubble, hubble_dot):
 
 
 ######################################################################
-# in H0, phi0 unit
+# in H0, omega0 unit
 def get_SR_epsilon(phi, inf_model):
     return get_SR_eta_planck(phi, inf_model) / inf_model.get_phi0()**2
 
@@ -42,22 +42,24 @@ def get_Hubble_SR_para(hubble, hubble_dot):
 ######################################################################
 
 
-def print_pr_para(inf_model):
+def print_pr_para(inf_model, phi, phi_dot):
     '''
     read CosmoLattice parameters
     in reduced Planck unit
     '''
-    phi0 = inf_model.get_phi0()
-    H0 = inf_model.get_H_inf()
+    phi0 = inf_model.get_phi0()  # in m_pl
+    phi_end = inf_model.get_phi_end()
     para_list = inf_model.get_ODE()
-    fStar = para_list[0] * phi0
-    omegaStar = 2.0 * np.sqrt(inf_model.get_d()) * fStar
+    fStar =  phi_end * phi0
+    #  omegaStar = 2.0 * np.sqrt(inf_model.get_d()) * fStar
+    omegaStar = inf_model.get_omegaStar()
 
     # simulation parameters in program units
-    t_max = para_list[1] / H0 * omegaStar
+    t_max = para_list[1]
     dt = t_max / para_list[2]
 
-    phi_dot_i = gl.get_phi_dot_SL_planck(fStar, inf_model) * gl.m_pl**2
+    # initial field velocity
+    phi_dot_end = np.interp(phi_end, phi[::-1], phi_dot[::-1]) * omegaStar * phi0
 
-    print("phiStar = %1.10e GeV, t_max = %e, dt = %e, init. mom. = %e GeV^2"
-          % (fStar*gl.m_pl, t_max, dt, phi_dot_i))
+    print("phiStar = %1.10e GeV, omegaStar = %1.10e GeV, t_max = %e, dt = %e, init. mom. = %e GeV^2, alpha = %f"
+          % (fStar*gl.m_pl, omegaStar*gl.m_pl, t_max, dt, phi_dot_end*gl.m_pl**2, inf_model.get_alpha()))
